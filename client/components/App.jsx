89f46261d481003c8c8753f6494705c0434d77b5
Keenen Leyson <keenen.leyson@gmail.com>
2020-09-22 16:08:58 +1200

    added timetable

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,8 @@ import Home from './Home'
 import Games from './Games'
 import Teams from './Teams'
 import TopNav from './TopNav'
+import DayOne from './DayOne'
+import DayTwo from './DayTwo'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/games" component={Games} />
         <Route exact path="/teams" component={Teams} />
+        <Route exact path="/games/day-one" component={DayOne} />
+        <Route exact path="/games/day-two" component={DayTwo} />
       </Router>
     </>
   )
---
+++ client/components/DayTwo.jsx
@@ -0,0 +1,147 @@
+import React from 'react'
+
+const DayTwo = () => {
+  return (
+    <>
+      <div className="col-1"></div>
+      <div className="limiter col-10">
+        <div className="container-table100">
+          <div className="wrap-table100">
+
+            <div className="table100 ver3 m-b-110">
+              <table data-vertable="ver3">
+                <thead>
+                  <tr className="row100 head">
+                    <th className="column100 column1" data-column="column1">DAY 2</th>
+                    <th className="column100 column2" data-column="column2">Court 1</th>
+                    <th className="column100 column3" data-column="column3">Court 2</th>
+                    <th className="column100 column4" data-column="column4">Court 3</th>
+                    <th className="column100 column5" data-column="column5">Court 4</th>
+                    <th className="column100 column6" data-column="column6">Court 5</th>
+                    <th className="column100 column7" data-column="column7">Court 6</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">8:00 AM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">9:00 AM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">10:00 AM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">11:00 AM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">12:00 AM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">AZT vs MAV | U19</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">1:00 PM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">2:00 PM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">3:00 PM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">4:00 PM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">5:00 PM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+
+                  <tr className="row100">
+                    <td className="column100 column1" data-column="column1">6:00 PM</td>
+                    <td className="column100 column2" data-column="column2">--</td>
+                    <td className="column100 column3" data-column="column3">--</td>
+                    <td className="column100 column4" data-column="column4">--</td>
+                    <td className="column100 column5" data-column="column5">--</td>
+                    <td className="column100 column6" data-column="column6">--</td>
+                    <td className="column100 column7" data-column="column7">--</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div className="col-1"></div>
+
+    </>
+  )
+}
+
+export default DayTwo
