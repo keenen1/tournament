98d02f32971b4fec9d4d7a4cbf7dea3395bb83d7
Keenen Leyson <keenen.leyson@gmail.com>
2020-09-23 16:06:26 +1200

    added info page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
+import TopNav from './TopNav'
 import Home from './Home'
 import Games from './Games'
-import Teams from './Teams'
-import TopNav from './TopNav'
 import DayOne from './DayOne'
 import DayTwo from './DayTwo'
+import Categories from './Categories'
 import Info from './Info'
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
       <Router>
         <Route exact path="/" component={Home} />
         <Route exact path="/games" component={Games} />
-        <Route exact path="/teams" component={Teams} />
+        <Route exact path="/teams" component={Categories} />
         <Route exact path="/games/day-one" component={DayOne} />
         <Route exact path="/games/day-two" component={DayTwo} />
         <Route exact path="/info" component={Info} />
--- client/components/TopNav.jsx
+++ client/components/TopNav.jsx
@@ -7,11 +7,11 @@ const TopNav = () => {
     <Router>
       <div className="top-nav">
         <Link to="/" className="col-2">HOME</Link>
+        <Link to="/info" className="col-2">INFO</Link>
         <Link to="/games" className="col-2">GAMES</Link>
         <Link to="/teams" className="col-2">CATEGORIES</Link>
         <Link to="/results" className="col-2">RESULTS</Link>
         <Link to="/news" className="col-2">NEWS</Link>
-        <Link to="/info" className="col-2">INFO</Link>
       </div>
     </Router>
     </>
