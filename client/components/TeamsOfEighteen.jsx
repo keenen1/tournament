24f6acebc06ee82d2a1df7e20315de55719fff95
Keenen Leyson <keenen.leyson@gmail.com>
2020-10-16 12:33:32 +1300

    omit pools for first publish

--- client/components/TeamsOfEighteen.jsx
+++ client/components/TeamsOfEighteen.jsx
@@ -4,21 +4,21 @@ const TeamsOfEighteen = () => {
   return (
     <div className="eighteen col-12">
       <div className="teams-block col-3">
-        <div className="pool">POOL A</div>
+        {/* <div className="pool">POOL A</div> */}
         <div className="team">UNKNOWN</div>
         <div className="team">SLAYERS</div>
         <div className="team">JBING</div>
         <div className="team">BRICK CITY</div>
       </div>
       <div className="teams-block col-3">
-        <div className="pool">POOL B</div>
+        {/* <div className="pool">POOL B</div> */}
         <div className="team">AZTIGS BETA</div>
         <div className="team">WOW 1</div>
         <div className="team">PINOY PROPERTY</div>
         <div className="team">MOUNT SISIG</div>
       </div>
       <div className="teams-block col-3">
-        <div className="pool">POOL C</div>
+        {/* <div className="pool">POOL C</div> */}
         <div className="team">PHENOM</div>
         <div className="team">TGIF</div>
         <div className="team">AUCKLANDER</div>
