5c1e114bcf5de0a1d4ba0c7531b59b1fe85a8f88
Keenen Leyson <keenen.leyson@gmail.com>
2020-10-23 16:01:13 +1300

    added rules

--- client/components/Rules.jsx
+++ client/components/Rules.jsx
@@ -3,9 +3,205 @@ import React from 'react'
 const Rules = () => {
   return (
     <div className="rules-page col-12">
-      <div className="rules col-6">
-        <div>Coming</div>
-        <div>Soon</div>
+      <div className="rules-content col-8">
+        <h1>BASKETBALL RULES</h1>
+        <h2>Team Players Eligibility</h2>
+        <ul>
+          <li>1. Only Filipino descent, spouse or partner of a Filipino will be allowed to play.</li>
+          <li>2. Every team will only be limited to field-in (allowed to play) 12 players in a game.</li>
+          <ul>
+            <li>
+              (a) The remaining members (max of 3) of a team will be marked as “alternate” and will
+              be the responsibility of the head coach to inform the alternate players that:</li>
+            <ul>
+              <li>(i) They will not be allowed to play in a particular game and</li>
+              <li>(ii) They will not be allowed to enter the playing court.</li>
+            </ul>
+            <li>
+              (b) If any of these “alternate” players are seen playing during a game, the following
+              penalty shall apply:
+            </li>
+            <li></li>
+            <ul>
+              <li>(i) Technical Foul on the Head Coach – Mark as “B”</li>
+              <li>(ii) Disqualifying foul on the said “reserve” player – Mark as “D”</li>
+              <li>(iii) The said “Reserve” player shall be removed from the Court immediately.</li>
+            </ul>
+            <li>
+              (c) After the team roster has been submitted to the governing official(s) of the
+              tournament, the roster of players cannot be changed after the deadline.
+            </li>
+            <li>
+              (d) Any player whose name appears in two or more teams in the same category shall
+              be considered as a player of the team where he first played. If the player decided
+              to play further with another team in the same category, the following penalty shall
+              be applied to the 2 nd and nth (team(s) he played after playing his first game with his
+              first team:
+            </li>
+            <ul>
+              <li>(i) The player will be banned for the rest of the league/tournament.</li>
+              <li>
+                (ii) The coach who accommodated the above player, 1 game
+                suspension.
+              </li>
+              <li>(iii) A coach with 2 suspensions – banned for the league/tournament.</li>
+              <li>(iv) ANY PROTEST ON A PLAYER’S ELIGIBILITY SHALL BE DONE BEFORE THE
+                START OF A GAME. A player under protest may continue to play unless
+                proven.
+              </li>
+              <li>
+                (v) Any protest done after the game has started will not affect the result of
+                the game. However, the Core Group will investigate and will apply what is
+                necessary on how the rules has been established.
+              </li>
+            </ul>
+          </ul>
+        </ul>
+        <br/>
+        <h2>In-House and FIBA rules in focus</h2>
+        <p>
+          Unless otherwise stated on this Competition Rules and Procedures, all (new) rules and
+          interpretations of the Auckland Labor Tournament Basketball Competitions shall follow the
+          latest adapted FIBA Rules applicable in New Zealand.
+        </p>
+        <p>
+          The primary focus of the association is the violent, unruly, abusive, insulting, trash talking
+          and other behavior of the Team (Coaching staff and Players), supporters, parents and spectators; that in the judgement of the association’s organizers, officials and advisers
+          affects the aim to unite the “Filipino Community” through sportsmanship and comradery.
+        </p>
+        <h3>The Games will be played as follows:</h3>
+        <ul>
+          <li>(a) All levels of competitions:</li>
+          <ul>
+            <li>1. Elimination to Quarter- Finals Round.</li>
+            <ul>
+              <li>(i) 20 minutes per half (running time)</li>
+              <li>(ii) 2x-30secs Time Outs per half (no carry-over from 1st to 2nd half)</li>
+              <li>(iii) No Time Outs on the last 3 mins of the first half</li>
+              <li>(iv) 7 Fouls - Penalty, Bonus shot starts on the 8 th fouls</li>
+              <li>(v) Last 1-min stop clock on the second half</li>
+              <li>
+                (vi) All Over Time (OT) goes to “Golden Basket Rule”- where the first team
+                to score wins the game; OT starts with a jump ball; all stats continue
+                with no time out.
+              </li>
+            </ul>
+            <li>
+              2. Semi-Finals and Finals format will be released before the start of the
+              tournament.
+            </li>
+            <ul>
+              <li>
+                (i) Last 3 minutes stop clock on 2nd half (Organizers reserve the right to
+                change the last minutes stop clock to better the competition.
+              </li>
+              <li>
+                (ii) First OT goes to the 2 mins Stop Clock with alternating possession
+                continued from the end of regulation; all stats continue.
+              </li>
+              <li>
+                (iii) 2nd Over (OT) goes to “Golden Basket Rule”- where the first team to
+                score wins the game; 2nd OT starts with a jump ball.
+              </li>
+            </ul>
+          </ul>
+          <li>(b) U15, U13 and U11 can apply the Man-to-Man (M2M) Defense Rule</li>
+        </ul>
+        <br/>
+        <h2>Focus on FIBA rules changes</h2>
+        <p>All new rule changes adopted by FIBA shall be applied as dated</p>
+        <br/>
+        <h2>Focus on In-house Rules</h2>
+        <ul>
+          <li>
+            <h3>
+              1. TECHNICALITIES - Trash Talking, Insults and Unnecessary Words or Gestures, etc…
+            </h3>
+          </li>
+          <ul>
+            <li>(i) First Team Violation, a player maybe given a Warning</li>
+            <li>(ii) Second Team Violation, a player will be given a Technical Foul</li>
+            <li>(iii) Third Team Violation, a player will be Ejected from the game (See Ejection)</li>
+          </ul>
+          <li>
+            <h3>
+              2. UNSPORTSMANLIKE BEHAVIOUR - excessive force, unnecessary 2nd motion, hard foul, etc…
+            </h3>
+          </li>
+          <ul>
+            <li>(i) First Player Violation - Unsportsmanlike Foul</li>
+            <li>(ii) Second Player Violation - Ejection from the game (See Ejection)</li>
+            <li>
+              (iii) Ejected player in every game will be under the evaluation of the Core Group for
+              suspension or ban for the rest of the tournament.
+            </li>
+            <li>
+              (iv) Any physical contact ( a punch, elbow, slap) by a player and/or a coaching staff that
+              in the judgement of the referee and/or of the Core Group was intended to harm or produce
+              physical injury shall be ejected from the game and will be ban for the tournament.
+            </li>
+          </ul>
+          <li>
+            <h3>
+              3. SUSPENSIONS
+            </h3>
+          </li>
+          <ul>
+            <li>1. Players ejected from the game due to:</li>
+            <li>2. Players 2x Ejected from the Tournament</li>
+            <li>
+              <p>
+                3. Players with 2 combined Technical, Unsportsmanlike especially of the same nature
+                will be under the evaluation of the Core Group.
+              </p>
+              <p>
+                Any player and/or any coaching staff that has been banned from the AFC tournament will
+                not be accepted from any tournament from affiliated association to AFC.
+              </p>
+              <p>
+                Every end of game, a referee must report to Head of Referee (Lemuel/designated) for any
+                technical and/or sportsmanship call made to a player.
+              </p>
+            </li>
+          </ul>
+          <li>
+            <h3>
+                4. FIGHTING
+            </h3>
+          </li>
+          <ul>
+            <li>
+                Any form of exchange physical contact with the intention to injure such as but not
+                limited to the following:
+            </li>
+            <ul>
+              <li>(i) Punching, Elbowing, Kicking, Head butting, etc</li>
+              <li>(i) Any form of fighting covered by FIBA Rules</li>
+              <li>(i) Any fight to any person inside the venue area and or</li>
+              <li>
+                  (i) Outside the venue area involving a fellow player, coaching staff, referees and the
+                  organizing body, and the Core Group… will also fall into this category.
+              </li>
+            </ul>
+          </ul>
+          <li>
+            <h3>
+                5. DEFAULTS AND FORFEITS
+            </h3>
+          </li>
+          <ul>
+            <li>
+              (i) If a team is unable to start the game as scheduled but managed to start within the
+              first 5 minutes of the game, 10pts will be added to present team and 15 mins left to
+              play the first half.
+            </li>
+            <li>
+              (ii) If a team is unable to start the game as scheduled and arrived more than 5 minutes
+              thereafter, then the referee shall forfeit the game and award a 20 pts to 0 to the
+              winning (present) team.
+            </li>
+          </ul>
+        </ul>
       </div>
     </div>
   )
