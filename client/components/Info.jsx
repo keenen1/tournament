61930360e2cc20979336953ef6bef11a3c578454
Keenen Leyson <keenen.leyson@gmail.com>
2020-11-18 00:40:18 +1300

    responsive map

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -11,7 +11,7 @@ const Info = () => {
         <div className="info-map col-12">
           <div>
             <iframe
-              className="map-box" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12781.918625780005!2d174.7437492!3d-36.7830485!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x151a15700a36587f!2sEventfinda%20Stadium!5e0!3m2!1sen!2snz!4v1605606888988!5m2!1sen!2snz" width="800vw" height="400vw" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0">
+              className="map-box" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12781.918625780005!2d174.7437492!3d-36.7830485!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x151a15700a36587f!2sEventfinda%20Stadium!5e0!3m2!1sen!2snz!4v1605606888988!5m2!1sen!2snz" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0">
             </iframe>
           </div>
           <a href="https://goo.gl/maps/vn3WzfeDxgw7ZZBH6">
